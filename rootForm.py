05c1e1747412e7279858d62485c03a269d61dc63
Omar Saaoui <omarsaaoui@yahoo.ca>
2014-11-12 20:46:07 -0500

    initial commit

---
+++ rootForm.py
@@ -0,0 +1,53 @@
+import os
+import re
+
+import jinja2
+import webapp2
+import cgi
+
+template_dir=os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env=jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+	autoescape=True)
+
+
+def root(word):
+	removeDuplicates= re.sub(r'([a-z])\1+', r'\1', word)
+	removeVowels= re.findall(r'[^aeiou]', removeDuplicates)
+	if removeVowels[0]=='t' and removeVowels[-1]=='t':
+		removeVowels=removeVowels[1:-1]
+	result= ''.join(removeVowels).lower()
+	return result
+jinja_env.globals['root']=root
+
+def render_str(template, **params):
+		t=jinja_env.get_template(template)
+		return t.render(params)
+
+class Handler(webapp2.RequestHandler):
+	def render(self, template, **kw):
+		self.response.out.write(render_str(template, **kw))
+
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+
+
+
+
+class rootForm(Handler):
+
+    
+    def get(self):
+        items=self.request.get_all('word')
+        self.render('root.html', items=items)
+        
+	
+
+
+	
+	
+
+
+app = webapp2.WSGIApplication([('/root', rootForm),
+			    ],
+			    debug=True)
+		
